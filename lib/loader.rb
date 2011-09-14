6a914066730ed25ca05d3e9cb17f3c7e283ea171
Maros Zatko <mzatko@redhat.com>
2011-09-15 00:59:03 +0200

    extremly primitive loading

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,12 +1,15 @@
 require 'yaml'
+require 'open-uri'
 
 module Rend
   class SceneLoader
     def initialize
     end
 
-    def load
-
+    def load filename
+      doc = YAML::load(open(filename))
+      doc ||= []
+      return doc
     end
   end
 end
