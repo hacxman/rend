d9957de50bd4391ac4adefa2841c74d2acf4c115
Maros Zatko <mzatko@redhat.com>
2011-09-17 01:24:26 +0200

    almost generic scene loading

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,6 +1,8 @@
 require 'yaml'
 require 'open-uri'
 
+require 'objects'
+
 module Rend
   class SceneLoader
     def initialize
@@ -9,7 +11,25 @@ module Rend
     def load filename
       doc = YAML::load(open(filename))
       doc ||= []
-      return doc
+      
+      scene = {}
+      doc.map do |item|
+        x = item.map do |j|
+          instn = nil
+          case j[0]
+          when "camera"
+            instn = Rend::Camera.new
+          when "sphere"
+            instn = Rend::Sphere.new
+          end
+          j[1].each do |var,val|
+            instn.instance_variable_set "@#{var}", val
+          end
+          instn
+        end [0]
+        {x.name, x}
+      end.reduce(Hash.new) {|a,b| a.merge b}
+
     end
   end
 end
