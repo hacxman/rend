ab0cc08b841e47e13abf2844b4ba9c8933baafae
Maros Zatko <mzatko@redhat.com>
2011-09-18 11:20:54 +0200

    ray

---
+++ lib/ray.rb
@@ -0,0 +1,17 @@
+module Rend
+  class Ray
+    attr_accessor :pos, :vec
+
+    def initialize(p, v)
+      @pos = p
+      @vec = v
+    end
+
+    def point_at(t)
+      p = Vector::elements @pos
+      v = Vector::elements @vec
+
+      return (p+v*t).to_a
+    end
+  end
+end
